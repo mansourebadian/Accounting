c7a5e826b982a798042a29887bb73f4ef5f4472c
MansourEbadian <m92ebadian@gmail.com>
2018-12-05 09:18:23 +0330

    create services folder and create CustomerRepository for implement ICustomerRepository

---
+++ Accounting/Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Repositories;
+using System.Data.Entity;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            try
+            {
+                var customer = GetCustomerById(customerId);
+                DeleteCustomer(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomerById(int customerId)
+        {
+            return db.Customers.Find(customerId);
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State=EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
